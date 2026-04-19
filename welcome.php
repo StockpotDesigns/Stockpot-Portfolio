b3d21c29e79cc28edfc21348c2cf391ec43ddc8a
StockpotDesigns <nheeldred14@gmail.com>
2026-04-19 01:48:29 -0600

    Add files via upload

---
+++ welcome.php
@@ -0,0 +1,12 @@
+<html lang="en">
+    <head>
+    <title>php</title>
+    </head>
+<body>
+
+Welcome <?php echo $_POST["firstname""lastname"]; ?><br>
+Your email address is: <?php echo $_POST["email"]; ?><br>
+Your message is: <?php echo $_POST["message"]; ?><br>
+
+</body>
+</html> 
